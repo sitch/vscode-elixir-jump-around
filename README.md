c9dfd3c3ea0f9a130e514fc7843d57c420d66900
Stephen Bussey <sb8244@cs.ship.edu>
2018-09-04 01:44:39 -0400

    It's all here

---
+++ README.md
@@ -0,0 +1,11 @@
+# elixir-jump-around README
+
+Elixir Jump Around is a very simple tool that enables one to quickly switch between a module file and that module's test file.
+
+The file that is switched to will use a very simple set of rules. If the file is located in test and ends in _test.exs, then the module file will be loaded by replacing "test" with "lib" in the path, and changing the file extension. Otherwise, "lib" is replaced with "test" in the path, and ".ex" is replaced with "_test.exs"
+
+The default keybinding is `alt+g alt+t`.
+
+## Inspiration
+
+This extension is a VSCode adaptation of valo's elixir-jump-around for atom. https://github.com/valo/elixir-jump-around
---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('extension.jump', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        const filePath = editor.document.uri.fsPath;
+        if (isTestFilePath(filePath)) {
+            const path = getModulePath(filePath);
+            createAndOpen(path);
+        } else {
+            const path = getTestPath(filePath);
+            createAndOpen(path);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
+
+function createAndOpen(path: string) {
+    const uri = vscode.Uri.file(path);
+    const edit = new vscode.WorkspaceEdit();
+    edit.createFile(uri, { ignoreIfExists: true, overwrite: false });
+    return vscode.workspace.applyEdit(edit).then(() => {
+        vscode.workspace.openTextDocument(uri).then((doc) => {
+            vscode.window.showTextDocument(doc);
+        }, () => {
+            vscode.window.showInformationMessage('Error switching files!');
+        });
+    });
+}
+
+function getModulePath(filePath: string) {
+    return filePath.replace("test", "lib").replace(/_test\.exs$/, ".ex");
+}
+
+function getTestPath(filePath: string) {
+    return filePath.replace("lib", "test").replace(/\.ex$/, "_test.exs");
+}
+
+function isTestFilePath(filePath: string) {
+    return filePath.match(/test\/.+_test\.exs$/);
+}
