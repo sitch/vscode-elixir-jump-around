f8849fc4d0e19a9130c50e358ee35d440fc6c821
sitch <2736163+sitch@users.noreply.github.com>
2019-08-06 16:39:10 -0400

    Didn't get around to writing the file contents
    This should setup a default test module content, just needs to implement line 35, should just be a one liner

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 'use strict';
 import * as vscode from 'vscode';
+import fs from "fs"
 
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('extension.jump', () => {
@@ -30,19 +31,36 @@ function createAndOpen(path: string) {
     edit.createFile(uri, { ignoreIfExists: true, overwrite: false });
     return vscode.workspace.applyEdit(edit).then(() => {
         vscode.workspace.openTextDocument(uri).then((doc) => {
-            vscode.window.showTextDocument(doc);
+             const contents = template(path)
+             vscode.window.showTextDocument(doc)
+                // TODO: IMPLEMENT
+                // .then(textEditor => {
+                //     // TODO: write file here
+                // })
         }, () => {
             vscode.window.showInformationMessage('Error switching files!');
         });
     });
 }
 
+function getModuleName(filePath: string) {
+    const contents = fs.readFileSync(filePath, 'utf8');
+    const matches = contents.match(/defmodule\s+(.+)\s+do\s/)
+    return matches[0]
+  }
+
+  function template(filePath: string) {
+    const moduleFilePath = getModulePath(filePath);
+    const module = getModuleName(moduleFilePath)
+    return `defmodule ${module}Test do\n  describe ".method/0" do\n    test "with valid params" do\n      raise "Not Implemented"\n    end\n  end\nend\n`
+  }
+
 function getModulePath(filePath: string) {
-    return filePath.replace("test", "lib").replace(/_test\.exs$/, ".ex");
+    return filePath.replace("test/lib", "lib").replace(/_test\.exs$/, ".ex");
 }
 
 function getTestPath(filePath: string) {
-    return filePath.replace("lib", "test").replace(/\.ex$/, "_test.exs");
+    return filePath.replace("lib", "test/lib").replace(/\.ex$/, "_test.exs");
 }
 
 function isTestFilePath(filePath: string) {
